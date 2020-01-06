41d02799a2ebccadc688deda7db5953682aad6c4
stevenkeezer <stevengkeezer@gmail.com>
2020-01-06 13:46:23 +0700

    testing if spinner is malfunctioning

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -18,7 +18,7 @@ export default function RecipeCard(props) {
 
   return (
     <>
-      {props.recipes.length < 1 && (
+      {/* {props.recipes.length < 1 && (
         <div className="d-flex mx-auto justify-content-center align-self-center">
           <CircularProgress
             style={{
@@ -27,7 +27,7 @@ export default function RecipeCard(props) {
             }}
           />
         </div>
-      )}
+      )} */}
 
       {props.recipes.map((r, i) => {
         if (i === 1) {
